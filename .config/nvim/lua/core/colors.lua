b74e2ac2f06773403146d7b76358d0b50deea8be
Pavel Miksha <89637241+keyboardcrash32@users.noreply.github.com>
2025-01-26 20:26:14 +0300

    Update colors.lua

--- .config/nvim/lua/core/colors.lua
+++ .config/nvim/lua/core/colors.lua
@@ -1,7 +1,7 @@
 vim.opt.termguicolors = true
 
 function SetColor(color)
-    color = color or "onedark"
+    color = color or "nightfox"
     vim.cmd.colorscheme(color)
 
     -- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
